c03284b50fa9a5deadda854b33fc5c7717f8ce13
Vlada <vladochka.vb@gmail.com>
2025-12-27 17:47:28 +1300

    add images

--- src/test/setupTests.ts
+++ src/test/setupTests.ts
@@ -1,11 +1,1 @@
-// import "@testing-library/jest-dom/vitest";
-// import { afterEach } from "vitest";
-// import { cleanup } from "@testing-library/react";
-
-// // Очистка после каждого теста
-// afterEach(() => {
-//   cleanup();
-// });
-
-
 import "@testing-library/jest-dom";
